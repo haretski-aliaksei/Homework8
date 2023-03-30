073db8fa7eb183fde6f078be1e353dece2dd388a
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-30 21:41:31 +0200

    update Credentials enum

--- src/test/java/enums/Credentials.java
+++ src/test/java/enums/Credentials.java
@@ -1,4 +1,17 @@
 package enums;
 
 public enum Credentials {
+    LOGIN("selenium_chrome"),
+    PASSWORD("Axa@Demo"),
+    INCORRECT_LOGIN("incorrectLogin");
+
+    private String credential;
+
+    Credentials(String credential) {
+        this.credential = credential;
+    }
+
+    public String getCredential() {
+        return credential;
+    }
 }
