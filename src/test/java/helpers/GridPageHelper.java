217df843588ba32e04560dde01f0662deb1aab0e
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-30 22:55:24 +0200

    update GridPageHelper

--- src/test/java/helpers/GridPageHelper.java
+++ src/test/java/helpers/GridPageHelper.java
@@ -1,11 +1,6 @@
 package helpers;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -21,22 +16,4 @@ public class GridPageHelper {
     public static final SelenideElement EXECUTION_REPORT_CONTAINER = $x("//div[contains(@class,'executions-grid')]/../..");
     public static final SelenideElement SUCCESS_MODAL_WINDOW = $x("//div[contains(@class, 'alert-success')]/..");
     public static final SelenideElement TOTAL_FIELD = $x("//div[@ref='eStatusBarLeft']//div[text()]");
-
-    public static List<String> getAnalystsItems() {
-        ElementsCollection items = Selenide.$$x("//div[contains(@class, 'align-items-center')]/b[@title]");
-        List<String> item = new ArrayList<>();
-        items.forEach(x -> item.add(x.getOwnText()));
-        return item;
-    }
-
-    public static List<String> getGridColumnItems() {
-        ElementsCollection items = Selenide.$$x("//div[@ref='eHeaderViewport']//div[@col-id]//div[@ref='eLabel']/span[text()]");
-        List<String> item = new ArrayList<>();
-        items.forEach(x -> item.add(x.getOwnText()));
-        return item;
-    }
-
-    public static void openActionButton(String actionName) {
-        $x("//div[@ref='eRightContainer']/div[@row-index='0']/div[@col-id='actions']//div/div[@title='" + actionName + "']").click();
-    }
 }
